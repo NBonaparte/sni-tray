286a5aa265d8d45b58584260260864ae8e00d180
NBonaparte <98007b33@opayq.com>
2017-07-31 21:46:46 -0700

    funky indents

--- draw.c
+++ draw.c
@@ -21,7 +21,7 @@ xcb_colormap_t colormap;
 uint8_t depth;
 
 typedef struct {
-		uint8_t r, g, b, a;
+	uint8_t r, g, b, a;
 } rgba_t;
 
 
@@ -53,7 +53,7 @@ xcb_visualtype_t* visual_type(xcb_screen_t* screen, int match_depth) {
 
 void mon_select(xcb_screen_t *s, xcb_rectangle_t *mon_dim, char *mon_name) {
 	xcb_randr_get_screen_resources_current_reply_t *r = xcb_randr_get_screen_resources_current_reply(c,
-		xcb_randr_get_screen_resources_current(c, s->root), NULL);
+			xcb_randr_get_screen_resources_current(c, s->root), NULL);
 	if(!r)
 		errx(1, "Failed to get screen resources");
 
@@ -61,37 +61,37 @@ void mon_select(xcb_screen_t *s, xcb_rectangle_t *mon_dim, char *mon_name) {
 	xcb_randr_output_t *o = xcb_randr_get_screen_resources_current_outputs(r);
 
 	if(mon_name != NULL) {
-	for(int i = 0; i < mon_total; i++) {
-		xcb_randr_get_output_info_reply_t *out = xcb_randr_get_output_info_reply(c,
-			xcb_randr_get_output_info(c, o[i], XCB_CURRENT_TIME), NULL);
+		for(int i = 0; i < mon_total; i++) {
+			xcb_randr_get_output_info_reply_t *out = xcb_randr_get_output_info_reply(c,
+					xcb_randr_get_output_info(c, o[i], XCB_CURRENT_TIME), NULL);
 
-		if(out == NULL || out->crtc == XCB_NONE || out->connection == XCB_RANDR_CONNECTION_DISCONNECTED)
-			continue;
+			if(out == NULL || out->crtc == XCB_NONE || out->connection == XCB_RANDR_CONNECTION_DISCONNECTED)
+				continue;
 
-		xcb_randr_get_crtc_info_reply_t *crtc = xcb_randr_get_crtc_info_reply(c,
-			xcb_randr_get_crtc_info(c, out->crtc, XCB_CURRENT_TIME), NULL);
-		if(!crtc) {
-			free(r);
-			errx(1, "Failed to get crtc info");
+			xcb_randr_get_crtc_info_reply_t *crtc = xcb_randr_get_crtc_info_reply(c,
+					xcb_randr_get_crtc_info(c, out->crtc, XCB_CURRENT_TIME), NULL);
+			if(!crtc) {
+				free(r);
+				errx(1, "Failed to get crtc info");
+			}
+
+			if(!strcmp(mon_name, (char *) xcb_randr_get_output_info_name(out))) {
+				*mon_dim = (xcb_rectangle_t){crtc->x, crtc->y, crtc->width, crtc->height};
+				break;
+			}
+			free(crtc);
+			free(out);
 		}
-
-		if(!strcmp(mon_name, (char *) xcb_randr_get_output_info_name(out))) {
-			*mon_dim = (xcb_rectangle_t){crtc->x, crtc->y, crtc->width, crtc->height};
-			break;
-		}
-		free(crtc);
-		free(out);
-	}
 	}
 	if(!mon_dim->width || !mon_dim->height) {
 		warnx("Using primary monitor as fallback");
 		// get primary output as fallback
 		xcb_randr_get_output_primary_reply_t *primary = xcb_randr_get_output_primary_reply(c,
-			xcb_randr_get_output_primary(c, s->root), NULL);
+				xcb_randr_get_output_primary(c, s->root), NULL);
 		xcb_randr_get_output_info_reply_t *pri = xcb_randr_get_output_info_reply(c,
-			xcb_randr_get_output_info(c, primary->output, XCB_CURRENT_TIME), NULL);
+				xcb_randr_get_output_info(c, primary->output, XCB_CURRENT_TIME), NULL);
 		xcb_randr_get_crtc_info_reply_t *crtc = xcb_randr_get_crtc_info_reply(c,
-			xcb_randr_get_crtc_info(c, pri->crtc, XCB_CURRENT_TIME), NULL);
+				xcb_randr_get_crtc_info(c, pri->crtc, XCB_CURRENT_TIME), NULL);
 		*mon_dim = (xcb_rectangle_t){crtc->x, crtc->y, crtc->width, crtc->height};
 		free(crtc);
 		free(pri);
@@ -110,7 +110,7 @@ void conf_win(xcb_screen_t *s, xcb_window_t w, xcb_rectangle_t *dim) {
 	xcb_ewmh_set_wm_state(ewmh, w, 2, test_atom);
 	xcb_ewmh_set_wm_window_type(ewmh, w, 1, &ewmh->_NET_WM_WINDOW_TYPE_DOCK);
 	xcb_ewmh_wm_strut_partial_t strut =
-		{0, 0, dim->height, 0, 0, 0, 0, 0, dim->x, dim->x + dim->width, 0, 0};
+	{0, 0, dim->height, 0, 0, 0, 0, 0, dim->x, dim->x + dim->width, 0, 0};
 	xcb_ewmh_set_wm_strut(ewmh, w, 0, 0, dim->height, 0);
 	xcb_ewmh_set_wm_strut_partial(ewmh, w, strut);
 
@@ -157,17 +157,17 @@ xcb_window_t main_win_init(xcb_screen_t *s, xcb_rectangle_t *dim) {
 	}
 	else
 		;
-		// TODO: switch to one version of visualtype function
-		//visual = get_visualtype(s);
+	// TODO: switch to one version of visualtype function
+	//visual = get_visualtype(s);
 
 	printf("depth: %d\n", depth);
 	//IMPORTANT: NEED TO DEFINE BACK AND BORDER PIXELS
 	uint32_t mask[] = {s->black_pixel, s->black_pixel, 1, XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_BUTTON_PRESS, colormap};
 
 	if(xcb_request_check(c, xcb_create_window_checked(c, depth, w, s->root,
-		dim->x, dim->y, dim->width, dim->height, 0,
-		XCB_WINDOW_CLASS_INPUT_OUTPUT, visual->visual_id,
-		XCB_CW_BACK_PIXEL | XCB_CW_BORDER_PIXEL | XCB_CW_OVERRIDE_REDIRECT | XCB_CW_EVENT_MASK | XCB_CW_COLORMAP, mask))!= NULL) {
+					dim->x, dim->y, dim->width, dim->height, 0,
+					XCB_WINDOW_CLASS_INPUT_OUTPUT, visual->visual_id,
+					XCB_CW_BACK_PIXEL | XCB_CW_BORDER_PIXEL | XCB_CW_OVERRIDE_REDIRECT | XCB_CW_EVENT_MASK | XCB_CW_COLORMAP, mask))!= NULL) {
 		errx(1, "dank");
 	}
 	char *title = "SNI Tray";
@@ -177,53 +177,53 @@ xcb_window_t main_win_init(xcb_screen_t *s, xcb_rectangle_t *dim) {
 	return w;
 }
 static cairo_surface_t * draw_surface_from_pixbuf(GdkPixbuf *buf) {
-    int width = gdk_pixbuf_get_width(buf);
-    int height = gdk_pixbuf_get_height(buf);
-    int pix_stride = gdk_pixbuf_get_rowstride(buf);
-    guchar *pixels = gdk_pixbuf_get_pixels(buf);
-    int channels = gdk_pixbuf_get_n_channels(buf);
-    cairo_surface_t *surface;
-    int cairo_stride;
-    unsigned char *cairo_pixels;
+	int width = gdk_pixbuf_get_width(buf);
+	int height = gdk_pixbuf_get_height(buf);
+	int pix_stride = gdk_pixbuf_get_rowstride(buf);
+	guchar *pixels = gdk_pixbuf_get_pixels(buf);
+	int channels = gdk_pixbuf_get_n_channels(buf);
+	cairo_surface_t *surface;
+	int cairo_stride;
+	unsigned char *cairo_pixels;
 
 
-    cairo_format_t format = CAIRO_FORMAT_ARGB32;
-    if (channels == 3)
-        format = CAIRO_FORMAT_RGB24;
+	cairo_format_t format = CAIRO_FORMAT_ARGB32;
+	if (channels == 3)
+		format = CAIRO_FORMAT_RGB24;
 
-    surface = cairo_image_surface_create(format, width, height);
-    cairo_surface_flush(surface);
-    cairo_stride = cairo_image_surface_get_stride(surface);
-    cairo_pixels = cairo_image_surface_get_data(surface);
+	surface = cairo_image_surface_create(format, width, height);
+	cairo_surface_flush(surface);
+	cairo_stride = cairo_image_surface_get_stride(surface);
+	cairo_pixels = cairo_image_surface_get_data(surface);
 
-    for (int y = 0; y < height; y++) {
-        guchar *row = pixels;
-        uint32_t *cairo = (uint32_t *) cairo_pixels;
-        for (int x = 0; x < width; x++) {
-            if (channels == 3) {
-                uint8_t r = *row++;
-                uint8_t g = *row++;
-                uint8_t b = *row++;
-                *cairo++ = (r << 16) | (g << 8) | b;
-            }
-            else {
-                uint8_t r = *row++;
-                uint8_t g = *row++;
-                uint8_t b = *row++;
-                uint8_t a = *row++;
-                double alpha = a / 255.0;
-                r = r * alpha;
-                g = g * alpha;
-                b = b * alpha;
-                *cairo++ = (a << 24) | (r << 16) | (g << 8) | b;
-            }
-        }
-        pixels += pix_stride;
-        cairo_pixels += cairo_stride;
-    }
+	for (int y = 0; y < height; y++) {
+		guchar *row = pixels;
+		uint32_t *cairo = (uint32_t *) cairo_pixels;
+		for (int x = 0; x < width; x++) {
+			if (channels == 3) {
+				uint8_t r = *row++;
+				uint8_t g = *row++;
+				uint8_t b = *row++;
+				*cairo++ = (r << 16) | (g << 8) | b;
+			}
+			else {
+				uint8_t r = *row++;
+				uint8_t g = *row++;
+				uint8_t b = *row++;
+				uint8_t a = *row++;
+				double alpha = a / 255.0;
+				r = r * alpha;
+				g = g * alpha;
+				b = b * alpha;
+				*cairo++ = (a << 24) | (r << 16) | (g << 8) | b;
+			}
+		}
+		pixels += pix_stride;
+		cairo_pixels += cairo_stride;
+	}
 
-    cairo_surface_mark_dirty(surface);
-    return surface;
+	cairo_surface_mark_dirty(surface);
+	return surface;
 }
 
 
@@ -244,7 +244,7 @@ cairo_surface_t *draw_pixmap(Pixmap *px) {
 	cairo_surface_t *ret = NULL;
 	//TODO check cairo format
 	ret = cairo_image_surface_create_for_data(px->pixmap, CAIRO_FORMAT_ARGB32, px->width, px->height,
-			cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, px->width));
+	cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, px->width));
 	return ret;
 }
 */
@@ -278,13 +278,13 @@ void init_window(win_data *data) {
 	/*
 	xcb_pixmap_t buf_pix = xcb_generate_id(c);
 	if(xcb_request_check(c, xcb_create_pixmap_checked(c, depth, buf_pix, w, win_dim.width, win_dim.height)))
-		errx(1, "Failed to create pixmap");
+	errx(1, "Failed to create pixmap");
 
 
 	xcb_visualtype_t *visual_tray = xcb_aux_find_visual_by_id(s, s->root_visual);
 	uint8_t depth = xcb_aux_get_depth_of_visual(s, visual->visual_id);
 	cairo_surface_t *buf = cairo_xcb_surface_create(c, buf_pix, visual, win_dim.width, win_dim.height);
-*/
+	*/
 	cairo_surface_t *surface = cairo_xcb_surface_create(c, w, visual, data->win_dim.width, data->win_dim.height);
 	//cairo_t *cr_buf = cairo_create(buf);
 	data->cr = cairo_create(surface);
@@ -294,7 +294,7 @@ void init_window(win_data *data) {
 	rgba_t bg = {0x00,0x00,0x00,0xaa};
 	cairo_reset_surface(data->cr, &bg);
 
-	draw_image(data->cr, "/home/michael/.icons/Flattr Dark/status/24/nm-signal-50.svg");
+	draw_image(data->cr, "/usr/share/icons/Papirus-Dark/24x24/panel/nm-signal-50.svg");
 }
 gboolean callback(xcb_generic_event_t *event, gpointer user_data) {
 	if(event == NULL) {
@@ -302,8 +302,21 @@ gboolean callback(xcb_generic_event_t *event, gpointer user_data) {
 		return FALSE;
 	}
 	switch (event->response_type & ~0x80) {
-		case XCB_BUTTON_PRESS:
-			printf("hai dere\n");
+		case XCB_BUTTON_PRESS: {
+			xcb_button_press_event_t *bp = (xcb_button_press_event_t *)event;
+			switch(bp->detail) {
+				// use event_x/y to determine which item was clicked, and use root_x/y as args
+				// for item methods
+				case 1:	//do primary click event (Activate)
+				case 2:	//do middle click event (SecondaryActivate)
+				case 3:	//do right click event (ContextMenu)
+				case 4:
+				case 5:	//scroll stuff (need to determine "delta" of scroll, use time/amount?
+				default:
+					printf("hi :3\n");
+			}
+			printf("%d pressed at %d, %d\n", bp->detail, bp->event_x, bp->event_y);
+		}
 	}
 	xcb_flush(c);
 	return TRUE;
